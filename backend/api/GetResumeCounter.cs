2f3fd403988b35687a3ca5e1bc511a485613a3d4
Onias Sayi <sayionias@gmail.com>
2026-01-26 09:16:17 +0200

    Updates to structure, created main.js file

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -1,35 +1,38 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.Functions.Worker;
+using System.Net.Http;
+using System.Text;
 
+
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
 namespace Company.Function
+#pragma warning restore IDE0130 // Namespace does not match folder structure damn this is annoying 
 {
     public static class GetResumeCounter
     {
         [FunctionName("GetResumeCounter")]
-        public static async Task<IActionResult> Run(
+        public static HttpResponseMessage Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDBInput(databaseName:"AzureResume", containerName:"Counter", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey ="1")] Counter counter,
+            [CosmosDBInput(databaseName:"AzureResume", containerName:"Counter", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey ="1")] out Counter updatedCounter,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            updatedCounter = counter;
+            updatedCounter.Count += 1;
+            var jasonToReturn = JsonConvert.SerializeObject(counter);
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = new StringContent(jasonToReturn, System.Text.Encoding.UTF8, "application/json")
+            };
         }
     }
 }
